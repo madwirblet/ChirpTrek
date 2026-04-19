be1a75d6dd81b44de04edc058bdceb953e91a05d
sophieshah <sophieshah922@gmail.com>
2026-04-19 12:13:15 -0400

    popups

---
+++ Assets/XRPopup.cs
@@ -0,0 +1,33 @@
+vusing UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
+
+public class XRPopup : MonoBehaviour
+{
+    public GameObject popupUI;
+    public TextMeshProUGUI popupText;
+
+    [TextArea]
+    public string message;
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
+
+    void Awake()
+    {
+        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+
+        interactable.hoverEntered.AddListener(ShowPopup);
+        interactable.hoverExited.AddListener(HidePopup);
+    }
+
+    void ShowPopup(HoverEnterEventArgs args)
+    {
+        popupText.text = message;
+        popupUI.SetActive(true);
+    }
+
+    void HidePopup(HoverExitEventArgs args)
+    {
+        popupUI.SetActive(false);
+    }
+}
